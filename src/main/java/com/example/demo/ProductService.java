998857968a743e786954e638847e8783183b4819
Joshua <jashuakogila6@gmail.com>
2024-06-14 18:36:29 +0530

    firtst commit

---
+++ src/main/java/com/example/demo/ProductService.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	public ProductRepository repository;
+
+
+	public Product saveProduct(Product product) {
+        return repository.save(product);
+    }
+
+    public List<Product> saveProducts(List<Product> products) {
+        return repository.saveAll(products);
+    }
+
+    public List<Product> getProducts() {
+        return repository.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return repository.findById((long) id).orElse(null);
+    }
+
+    public Product getProductByName(String name) {
+        return repository.findByName(name);
+    }
+
+    public String deleteProduct(int id) {
+        repository.deleteById((long) id);
+        return "product removed !! " + id;
+    }
+
+    public Product updateProduct(Product product) {
+        Product existingProduct = repository.findById((long) product.getId()).orElse(null);
+        existingProduct.setName(product.getName());
+        existingProduct.setQuantity(product.getQuantity());
+        existingProduct.setPrice(product.getPrice());
+        return repository.save(existingProduct);
+    }
+}
+	
+
